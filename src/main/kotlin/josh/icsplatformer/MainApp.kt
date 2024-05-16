8bf2aa8e40c6fb737df93ce6d25cf7b14a3a55ec
Joshua-Deering <jidjosh2006@icloud.com>
2024-05-15 22:18:19 -0400

    Initial Commit

--- src/main/kotlin/josh/icsplatformer/MainApp.kt
+++ src/main/kotlin/josh/icsplatformer/MainApp.kt
@@ -3,7 +3,6 @@ package josh.icsplatformer
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
-import javafx.scene.canvas.Canvas
 import javafx.stage.Stage
 
 class MainApp : Application() {
@@ -13,8 +12,8 @@ class MainApp : Application() {
         stage.title = "ICS-Platformer"
         stage.scene = scene
 
-        val gameloop = GameLoop(fxmlLoader.getController<Controller>().gameCanvas.graphicsContext2D);
-        gameloop.start();
+        val gameloop = GameLoop(fxmlLoader.getController<Controller>().gameCanvas.graphicsContext2D)
+        gameloop.start()
 
         stage.show()
     }
