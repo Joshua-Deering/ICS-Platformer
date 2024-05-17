3830c6d5c7d62cdb4d9b9fb406b6192217e9514e
Joshua-Deering <jidjosh2006@icloud.com>
2024-05-16 23:03:47 -0400

    Documented Player and Hitbox classes

--- src/main/kotlin/josh/icsplatformer/lib/Hitbox.kt
+++ src/main/kotlin/josh/icsplatformer/lib/Hitbox.kt
@@ -1,9 +1,19 @@
 package josh.icsplatformer.lib
 
+/**
+ * Hitbox class for storing information about entities collision areas.
+ *
+ * @property min The top left corner of this Hitbox
+ * @property max The bottom right corner of this Hitbox
+ */
 data class Hitbox(var min: Vec2 = Vec2(), var max: Vec2 = Vec2()) {
     val width = max.x - min.x
     val height = max.y - min.y
 
+    /**
+     * Checks whether this Hitbox overlaps with another Hitbox
+     * @param other Other Hitbox
+     */
     fun overlaps(other: Hitbox): Boolean {
         val d1x = other.min.x - min.x
         val d1y = other.min.y - min.y
@@ -13,6 +23,10 @@ data class Hitbox(var min: Vec2 = Vec2(), var max: Vec2 = Vec2()) {
         return !(d1x > 0.0 || d1y > 0.0 || d2x > 0.0 || d2y > 0.0)
     }
 
+    /**
+     * Moves this hitbox by some amount
+     * @param dist How much to move
+     */
     fun move(dist: Vec2) {
         min.plusAssign(dist)
         max.plusAssign(dist)
