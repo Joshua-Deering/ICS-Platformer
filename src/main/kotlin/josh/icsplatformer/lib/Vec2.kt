419197ca01e3409cd333e8715180dd0c24b4331d
Joshua-Deering <jidjosh2006@icloud.com>
2024-05-21 20:34:06 -0400

    Added Vec2.clamp to limit velocity

--- src/main/kotlin/josh/icsplatformer/lib/Vec2.kt
+++ src/main/kotlin/josh/icsplatformer/lib/Vec2.kt
@@ -1,5 +1,8 @@
 package josh.icsplatformer.lib
 
+import kotlin.math.max
+import kotlin.math.min
+
 data class Vec2(var x: Double = 0.0, var y: Double = 0.0) {
 
     operator fun plus(other: Vec2) = Vec2(x + other.x, y + other.y)
@@ -16,5 +19,10 @@ data class Vec2(var x: Double = 0.0, var y: Double = 0.0) {
     fun scalarMult(other: Double) {
         x *= other
         y *= other
+    }
+
+    fun clamp(min: Vec2, max: Vec2) {
+        x = min(max(x, min.x), max.x)
+        y = min(max(y, min.y), max.y)
     }
 }
